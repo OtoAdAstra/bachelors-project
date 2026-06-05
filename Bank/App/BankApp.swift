2b544dcdb227afd20e5a05aabcd7122558074939
Oto <sharvashidze0712@gmail.com>
2026-06-06 03:31:02 +0400

    feat: configure firebase and folder structure

--- Bank/App/BankApp.swift
+++ Bank/App/BankApp.swift
@@ -1,7 +1,12 @@
 import SwiftUI
+import FirebaseCore
 
 @main
 struct BankApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
